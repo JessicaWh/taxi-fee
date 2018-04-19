040ec685be18b0998df082ec0b54e23f3200d52b
JessicaWh <1030918615@qq.com>
2018-04-19 15:28:01 +0800

    提交

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
-module.exports = function main() {
-    console.log("Debug Info");
+module.exports = function main(kilo,waitMin) {
+    var str = 0;
+    if( kilo <=2 ){
+        str = 6;
+    }
+    else if(kilo > 2 && kilo < 8){
+        str = 6 + (kilo -2) * 0.8;
+    }
+    else{
+        str = 6 + (8-2) * 0.8 +(kilo - 8)*(0.8+0.8/2);
+    }
+    str += waitMin*0.25;
+    console.log(Math.round(str));
     return 'Hello World!';
 };
